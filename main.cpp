a05e972ef57f91ad8dcb7406e852c3d259b7dd45
Pr1ncei <princei.cs50@gmail.com>
2025-11-29 11:34:52 +0800

    Added C++ Template

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using uint = unsigned int; 
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll,ll>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x.size())
+#define each(x,a) for (auto &x : a)
+#define fr(i, a, b) for (int i = (a); i < (b); i++)
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1; 
+    // Uncomment if requires test cases
+    // cin >> t;
+    while (t--) solve();
+    return 0; 
+}
